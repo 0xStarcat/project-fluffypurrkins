6f881d484bcd2b539cd530d3fbaad9ac3a9d7e42
Jeff <jeffahking@gmail.com>
2017-07-04 13:08:22 -0400

    make contact font larger

--- src/Client/Pages/Contact.react.js
+++ src/Client/Pages/Contact.react.js
@@ -12,17 +12,17 @@ export default class Contact extends Component {
         <div className='page'>
           
           <section>
-            <div className='sub-section'>
-              <p className='sub-header linky'><a href='https://github.com/0xStarcat' target='_blank'>Github</a></p>
+            <div className='sub-section text-body'>
+              <p className='sub-header linky large-text'><a href='https://github.com/0xStarcat' target='_blank'>Github</a></p>
             </div>
-            <div className='sub-section'>
-              <p className='sub-header linky'><a href='https://twitter.com/0xStarcat' target='_blank'>Twitter</a></p>
+            <div className='sub-section text-body'>
+              <p className='sub-header linky large-text'><a href='https://twitter.com/0xStarcat' target='_blank'>Twitter</a></p>
             </div>
-            <div className='sub-section'>
-              <p className='sub-header linky'><a href="mailto:jeffahking@gmail.com" target="_top">jeffahking@gmail.com</a></p>
+            <div className='sub-section text-body'>
+              <p className='sub-header linky large-text'><a href="mailto:jeffahking@gmail.com" target="_top">jeffahking@gmail.com</a></p>
             </div>
-            <div className='sub-section'>
-              <p className='sub-header'>PGP</p>
+            <div className='sub-section text-body'>
+              <p className='sub-header large-text'>PGP</p>
               <p>264D 87F7 C02E 2EC5 079D  ABE0 34CC 90E8 07AF CE6E</p>
             </div>
           </section>
