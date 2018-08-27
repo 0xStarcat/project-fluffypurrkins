b6970744170f2552ddad16ea16573bb55f994d82
angel <xxStarcat@gmail.com>
2018-08-26 23:09:42 -0400

    style

--- src/Pages/Projects/ProjectItem/index.js
+++ src/Pages/Projects/ProjectItem/index.js
@@ -16,29 +16,31 @@ const ProjectItem = props => {
 
   return (
     <div className={classnames('project-item', { selected: props.active })}>
-      <div className="inline-wrapper" onTouchStart={selectItem} onClick={selectItem}>
+      <div className="inline-wrapper" onTouchEnd={selectItem} onClick={selectItem}>
         <div className="border-container">
-          <div className={'list-border top-border'} />
+          <div className={classnames('list-border top-border', { 'last-border': props.last })} />
           {props.last ? null : <div className="list-border bottom-border" />}
         </div>
         <div className="project-item-text">{props.project.title}</div>
       </div>
       <div className="project-item-full-wrapper">
-        <div className={classnames('project-item-full', { 'no-border': props.last })}>
-          <div
-            className={classnames('project-item-full-inner-content', { 'no-border': props.last })}
-          >
-            {props.project.mainImage && (
-              <img
-                alt="NYC Building Monitor image"
-                className="project-image"
-                src={`https://cms.starcat.xyz${props.project.mainImage.url}`}
-              />
-            )}
-            <a className="project-content" href={`${props.project.link}`}>
-              View this project
-            </a>
-            {props.project.generalDescription && <p>{props.project.generalDescription}</p>}
+        <div className={classnames('project-item-full')}>
+          <div className={classnames('project-item-content-wrapper', { 'no-border': props.last })}>
+            <div className="project-item-content">
+              {props.project.mainImage && (
+                <img
+                  alt="NYC Building Monitor image"
+                  className="project-image project-content-item"
+                  src={`https://cms.starcat.xyz${props.project.mainImage.url}`}
+                />
+              )}
+              <a className="project-link project-content-item" href={`${props.project.link}`}>
+                View this project
+              </a>
+              {props.project.generalDescription && (
+                <p className="project-content-item">{props.project.generalDescription}</p>
+              )}
+            </div>
           </div>
         </div>
       </div>
