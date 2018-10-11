84525194ac09e5a7901c4ed4e1a7f71868d4f1f1
angel <xxStarcat@gmail.com>
2018-10-10 20:48:01 -0400

    update

--- src/Pages/Contact.react.js
+++ src/Pages/Contact.react.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PageHeader from './PageHeader'
 import './Style/page.scss'
 export default class Contact extends Component {
@@ -6,24 +6,31 @@ export default class Contact extends Component {
     super()
   }
   render() {
-    return(
+    return (
       <div>
-        <PageHeader/>
-        <div className='page'>
-          
+        <PageHeader />
+        <div className="page">
           <section>
-            <div className='sub-section text-body'>
-              <p className='sub-header linky large-text'><a href='https://github.com/0xStarcat' target='_blank'>Github</a></p>
+            <div className="sub-section text-body">
+              <p className="sub-header linky large-text">
+                <a href="https://github.com/0xStarcat" target="_blank">
+                  Personal Github
+                </a>
+              </p>
             </div>
-            <div className='sub-section text-body'>
-              <p className='sub-header linky large-text'><a href='https://twitter.com/0xStarcat' target='_blank'>Twitter</a></p>
+            <div className="sub-section text-body">
+              <p className="sub-header linky large-text">
+                <a href="mailto:jadeahking@gmail.com" target="_top">
+                  jadeahking@gmail.com
+                </a>
+              </p>
             </div>
-            <div className='sub-section text-body'>
-              <p className='sub-header linky large-text'><a href="mailto:jadeahking@gmail.com" target="_top">jadeahking@gmail.com</a></p>
-            </div>
-            <div className='sub-section text-body'>
-              <p className='sub-header large-text'>PGP</p>
-              <p>264D 87F7 C02E 2EC5 079D  ABE0 34CC 90E8 07AF CE6E</p>
+            <div className="sub-section text-body">
+              <p className="sub-header large-text">
+                <a href="https://keybase.io/starcat" target="_blank">
+                  Keybase
+                </a>
+              </p>
             </div>
           </section>
         </div>
